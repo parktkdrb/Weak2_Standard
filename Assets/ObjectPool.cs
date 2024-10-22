1dc6e61b579e1c0d586b8d4b4e86356817e79b8f
parktkdrb <qkrtkdrb05@naver.com>
2024-10-22 20:24:28 +0900

    오브젝트 풀링 40%

--- Assets/ObjectPool.cs
+++ Assets/ObjectPool.cs
@@ -11,18 +11,31 @@ public class ObjectPool : MonoBehaviour
     void Start()
     {
         //미리 poolSize만큼 게임오브젝트를 생성한다.
-        pool.Clear();
-        
+        for(int i = 0; i < poolSize; i++)
+        {
+            pool.Add(Instantiate(prefab));
+        }
+            
         
     }
 
-    /*public GameObject Get()
-    {
+    public GameObject Get()
+    { 
         // 꺼져있는 게임오브젝트를 찾아 active한 상태로 변경하고 return 한다.
+        for (int i = 0; i < pool.Count; i++)
+        {
+            prefab = pool[i].gameObject;
+            if (prefab.activeSelf == false)
+            {
+                prefab.SetActive(true);
+            }
+        }
+        return prefab;
     }
 
     public void Release(GameObject obj)
     {
         // 게임오브젝트를 deactive한다.
-    }*/
+        obj.SetActive(false);
+    }
 }
